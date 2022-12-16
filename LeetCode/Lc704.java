15b8e0327dea5bcaa42fae1e556f5f795d7de718
Yung Cho <yung5060@kakao.com>
2022-12-16 15:57:26 +0900

    LeetCode Start!

---
+++ LeetCode/Lc704.java
@@ -0,0 +1,23 @@
+public class Lc704 {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, mid;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { -1, 0, 3, 5, 9, 1 };
+        Lc704 tmp = new Lc704();
+        System.out.println(tmp.search(nums, 2));
+    }
+}
